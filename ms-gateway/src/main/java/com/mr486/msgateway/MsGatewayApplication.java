df586f9b4986b83f019ba43d0ed0c0cc4d73cff4
monsieur486 <monsieur486@gmail.com>
2026-07-25 17:06:51 +0200

    docs(gateway): Javadoc française et YAML commenté

--- ms-gateway/src/main/java/com/mr486/msgateway/MsGatewayApplication.java
+++ ms-gateway/src/main/java/com/mr486/msgateway/MsGatewayApplication.java
@@ -3,11 +3,26 @@ package com.mr486.msgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Point d'entrée de la passerelle (Spring Cloud Gateway) : route les requêtes
+ * vers les microservices enregistrés auprès d'Eureka.
+ *
+ * <p><b>Exemple :</b> lancer la méthode main démarre la passerelle sur le port
+ * configuré ; GET /ms-patients/patients est routé vers ms-patients.</p>
+ */
 @SpringBootApplication
 public class MsGatewayApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(MsGatewayApplication.class, args);
-  }
+    /**
+     * Démarre l'application Spring Boot.
+     *
+     * <p><b>Exemple :</b> main(new String[0]) lance la passerelle sur le port
+     * configuré.</p>
+     *
+     * @param args les arguments de la ligne de commande
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MsGatewayApplication.class, args);
+    }
 
 }
--- ms-gateway/src/test/java/com/mr486/msgateway/MsGatewayApplicationTests.java
+++ ms-gateway/src/test/java/com/mr486/msgateway/MsGatewayApplicationTests.java
@@ -3,11 +3,18 @@ package com.mr486.msgateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Vérifie que le contexte Spring de la passerelle démarre correctement.
+ *
+ * <p><b>Exemple :</b> contextLoads() échoue si un bean de la passerelle ne peut
+ * pas être construit.</p>
+ */
 @SpringBootTest
 class MsGatewayApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+    @Test
+    void contextLoads() {
+        // le chargement du contexte suffit : un échec de configuration fait échouer le test
+    }
 
 }
