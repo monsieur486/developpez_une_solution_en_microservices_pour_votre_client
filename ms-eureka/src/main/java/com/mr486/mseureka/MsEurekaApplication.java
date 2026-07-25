f8aca41608ded9c4746ccae5617159d42e55a371
monsieur486 <monsieur486@gmail.com>
2026-07-25 17:06:51 +0200

    docs(eureka): Javadoc française et YAML commenté

--- ms-eureka/src/main/java/com/mr486/mseureka/MsEurekaApplication.java
+++ ms-eureka/src/main/java/com/mr486/mseureka/MsEurekaApplication.java
@@ -4,12 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Point d'entrée du serveur d'annuaire Eureka : les microservices s'y
+ * enregistrent et s'y découvrent mutuellement.
+ *
+ * <p><b>Exemple :</b> lancer la méthode main démarre l'annuaire ; le tableau de
+ * bord Eureka est alors accessible sur le port configuré.</p>
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class MsEurekaApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(MsEurekaApplication.class, args);
-  }
+    /**
+     * Démarre l'application Spring Boot.
+     *
+     * <p><b>Exemple :</b> main(new String[0]) lance l'annuaire Eureka sur le
+     * port configuré.</p>
+     *
+     * @param args les arguments de la ligne de commande
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MsEurekaApplication.class, args);
+    }
 
 }
--- ms-eureka/src/test/java/com/mr486/mseureka/MsEurekaApplicationTests.java
+++ ms-eureka/src/test/java/com/mr486/mseureka/MsEurekaApplicationTests.java
@@ -3,11 +3,18 @@ package com.mr486.mseureka;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Vérifie que le contexte Spring du serveur Eureka démarre correctement.
+ *
+ * <p><b>Exemple :</b> contextLoads() échoue si un bean de l'annuaire ne peut
+ * pas être construit.</p>
+ */
 @SpringBootTest
 class MsEurekaApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+    @Test
+    void contextLoads() {
+        // le chargement du contexte suffit : un échec de configuration fait échouer le test
+    }
 
 }
